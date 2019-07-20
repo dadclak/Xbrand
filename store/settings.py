ead65c95474e2ee961c745df68a11f27bc88a7d3
dadclak <kozanchyn.vladeo@gmail.com>
2019-07-20 14:26:27 +0200

    styly home page

--- store/settings.py
+++ store/settings.py
@@ -128,6 +128,6 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'kozanchyn.vladeo@gmail.com'
-EMAIL_HOST_PASSWORD  =  'the_dadclak@yarmak'
+EMAIL_HOST_PASSWORD  =  'kniaz_vk@dadclak26'
 SERVER_EMAIL = EMAIL_HOST_USER
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
